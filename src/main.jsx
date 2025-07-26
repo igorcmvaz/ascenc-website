0a223a695a89de32e63ae059b09eec74eed4bacd
Cassio Schmidt <cassioschii@gmail.com>
2025-07-26 09:03:46 -0300

    feat: Add React Router Dom to main file and route pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import About from "./pages/About";
+import "./index.css";
+import Home from "./pages/Home";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
